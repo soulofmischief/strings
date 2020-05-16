3fe0db1903be7414a9e2f315945c2e0c3444f8c0
soulofmischief <soulofmischief@protonmail.com>
2020-05-15 19:09:54 -0500

    Add tests for shortID

---
+++ tests/shortID.js
@@ -0,0 +1,19 @@
+import o from 'ospec'
+import { shortID } from '../src'
+
+
+const
+  id1 = shortID(),
+  id2 = shortID()
+
+
+o.spec( 'shortID', () => {
+  o( 'is unique', () => {
+    o( id1 ).notEquals( id2 )
+  })
+
+  o( 'is six letters, digits, hyphens or underscores', () => {
+    o( /^[a-zA-Z0-9\-_]{6}$/.test( id1 )).equals( true )
+    o( /^[a-zA-Z0-9\-_]{6}$/.test( id2 )).equals( true )
+  })
+})
