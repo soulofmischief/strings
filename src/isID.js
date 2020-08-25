04feb7857a7d8b73e52eb9e708b60e5064528e1d
soulofmischief <soulofmischief@protonmail.com>
2020-08-25 01:49:55 -0500

    Add isID()

---
+++ src/isID.js
@@ -0,0 +1,5 @@
+// @flow strict
+import { id } from '@soulofmischief/regex'
+
+
+export function isID( s: string ) { return id.test( s )}
---
+++ tests/isID.js
@@ -0,0 +1,23 @@
+import o from 'ospec'
+import { isID } from '../src'
+
+const
+  id = '1234567890abcdef',
+  invalid = '!@#$%^&*()',
+  tooLong = `${id}g`,
+  tooShort = `${id.substr( 1 )}`
+
+o.spec( 'isID', () => {
+  o( 'matches ID', () => {
+    o( isID( id )).equals( true )
+  })
+
+  o( "doesn't match non-alphanumerical string", () => {
+    o( isID( invalid )).equals( false )
+  })
+
+  o( "doesn't match wrong length", () => {
+    o( isID( tooLong )).equals( false )
+    o( isID( tooShort )).equals( false )
+  })
+})
